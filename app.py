bfc08bc146faaa2e8c806e73a9a163a551eea44f
Daniella Machado <seu-email@exemplo.com>
2025-10-07 19:42:19 -0300

    forms fixed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import csv
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -31,6 +32,43 @@ def book_details(book_id):
     else:
         return "Book not found!", 404
 
+@app.get("/add-review")
+def show_add_form():
+    return render_template("add-review.html")
+
+@app.post("/add-review")
+def add_review():
+
+    with open("books.csv", mode="r", encoding="utf-8") as csv_file:
+        reader = list(csv.reader(csv_file))
+        last_id = 0
+
+        if len(reader) > 1:
+            last_row = reader[-1]
+            if last_row:
+                last_id = int(last_row[0])
+
+        next_id = last_id + 1
+
+    watched_date_raw = request.form.get("watched_date")
+    date_object = datetime.strptime(watched_date_raw, "%Y-%m-%d")
+
+    formatted_date = date_object.strftime("%m/%d/%Y")
+
+    new_book_data = [
+        next_id,
+        request.form.get("title"),
+        request.form.get("poster_url"),
+        request.form.get("rating"),
+        formatted_date,
+        request.form.get("review"),
+    ]
+
+    with open("books.csv", mode="a", encoding="utf-8", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(new_book_data)
+
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
