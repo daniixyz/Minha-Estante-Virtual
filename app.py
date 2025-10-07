632f903d0fde89c5ba120785f35d12d148b59880
Daniella Machado <seu-email@exemplo.com>
2025-10-07 17:46:29 -0300

    backend to details pages

--- app.py
+++ app.py
@@ -13,5 +13,24 @@ def index():
 
     return render_template("index.html", books=books)
 
+@app.get("/book/<int:book_id>")
+def book_details(book_id):
+    found_book = None
+
+    with open('books.csv', mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        for book in csv_reader:
+            if int(book['id']) == book_id:
+                found_book = book
+                break
+    
+    if found_book:
+        return render_template("details.html", book=found_book)
+
+    else:
+        return "Book not found!", 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
